41918a4a0e231c6ac8d94f636fd510215341e13e
YuriiTovmach <portfolio_yurii_tovmach@ukr.net>
2020-03-17 15:33:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+// получить и вывести
+
+     echo 'time_zone - ' . file_get_contents('https://ipapi.co/timezone/');
+     echo '<br>';
+     echo  'country_code - ' . file_get_contents('https://ipapi.co/country_code/');
+
+     
